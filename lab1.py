a84a6999a3516969cbcd51e98809981ebfd87420
P4U113 <paulflorin.neaga@ulbsibiu.ro>
2026-03-11 17:36:23 +0200

    a

---
+++ lab1.py
@@ -0,0 +1,25 @@
+name='Paul';
+age=21
+print(f'eu sunt {name}')
+print(f'eu am {age} de ani')
+print(2+5)
+print(5/5)
+print(5*5)
+print('I/`m thirsty')
+if(7>4):
+    print('mai mare')
+else:
+    print('mai mic')
+
+if (2 > 4 and 8>4) :
+        print('adv')
+else:
+        print('fls')
+
+print('i`m alone     '.strip())
+print('i-m alone'.strip('a'))
+print('afdsdaw dawdafds fsefsdfse sefefs'.split())
+
+a='afdsdaw dawdafds fsefsdfse sefefs'.split()
+
+print(len(a))
