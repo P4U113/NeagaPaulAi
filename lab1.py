dcbf56ed3435b7ee41c138595d152b0e6ab8d9c7
P4U113 <paulflorin.neaga@ulbsibiu.ro>
2026-03-11 17:40:42 +0200

    3/11/2026

--- lab1.py
+++ lab1.py
@@ -21,5 +21,11 @@ print('i-m alone'.strip('a'))
 print('afdsdaw dawdafds fsefsdfse sefefs'.split())
 
 a='afdsdaw dawdafds fsefsdfse sefefs'.split()
+b='jnewjaneja w dikajwdoiawd wadioahnwd'.split()
 
 print(len(a))
+
+
+print(bool(2>1))
+print(bool(2>6))
+print(bool(len(a)>len(b)))
