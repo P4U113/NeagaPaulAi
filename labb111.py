c9c05db8cf65d87b82cf4a9d6d3a8a6704cacb2e
P4U113 <paulflorin.neaga@ulbsibiu.ro>
2026-06-03 11:36:16 +0300

    regresie liniara 5

--- labb111.py
+++ labb111.py
@@ -1,5 +1,7 @@
 from sklearn.linear_model import LinearRegression
 from sklearn.datasets import load_diabetes
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error
 diabetes=load_diabetes()
 import pandas as pd
 import numpy as np
